10f99c8dba7d0f5f86ef5671792ddcf1a6a95acf
Mostafa Mohamed <77597243+mostafa6679@users.noreply.github.com>
2021-08-23 20:24:47 -0400

    Add files via upload

---
+++ MOSTAFA/index.js
@@ -0,0 +1,7 @@
+$(function () {
+$('#MOSTAFA,#mostafa').addClass("active");
+$('.twitter').addClass('active2');
+$('.facebook').addClass('active3');
+$('.youtube,.instagram,.tiktok').addClass('active4');
+
+})
